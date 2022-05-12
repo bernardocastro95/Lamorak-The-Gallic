25250de284acf89b25a6dd204fd08e474d8f9b95
Bernardo Castro <bernardoc544@gmail.com>
2022-05-12 18:29:46 -0400

    Transition Between Scenes Script

---
+++ Olympian-Redemption/Assets/Scripts/IntroButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Button beginButton, backButton;
+
+    public void loadGame()
+    {
+        beginButton.onClick.AddListener(level1);
+        backButton.onClick.AddListener(backMainMenu);
+    }
+
+    public void level1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void backMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Olympian-Redemption/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton, exitButton;
+
+    public void loadGame()
+    {
+        startButton.onClick.AddListener(startGame);
+        exitButton.onClick.AddListener(exitGame);
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
