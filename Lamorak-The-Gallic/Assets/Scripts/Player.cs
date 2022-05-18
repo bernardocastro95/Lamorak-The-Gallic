da323b66c435e47392165d8f654b3bbfda9d39aa
Bernardo Castro <bernardoc544@gmail.com>
2022-05-18 18:13:01 -0400

    Glitches fixed and starting on jump mechanic

--- Lamorak-The-Gallic/Assets/Scripts/Player.cs
+++ Lamorak-The-Gallic/Assets/Scripts/Player.cs
@@ -8,15 +8,16 @@ public class Player : MonoBehaviour
     
 {
     public float speed = 0;
-    bool jump = false;
     Rigidbody2D r2d;
     float inputX, inputY;
+    
 
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         r2d = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -24,27 +25,28 @@ public class Player : MonoBehaviour
     {
 
         animator.SetFloat("GroundSpeed", Mathf.Abs(speed));
+        animator.SetBool("isJumping", false);
 
         inputX = Input.GetAxisRaw("Horizontal");
         inputY = Input.GetAxisRaw("Vertical");
 
+        Movement();
+        BackToMainMenu();
+        Jump();
+        
+    }
 
-        /*Vector3 move = new Vector3(speed * inputX, 0, 0);
+    void FixedUpdate()
+    {
+        r2d.velocity = new Vector2(speed * inputX, speed * inputY);
+    }
 
-        move *= Time.deltaTime;
-
-        transform.Translate(move);
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-            Debug.Log("Jumping");
-        }*/
-
+    void Movement()
+    {
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             speed += 10;
-            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            gameObject.transform.localScale = new Vector3(.5f, .5f, .5f);
         }
 
         if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
@@ -55,25 +57,30 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             speed += 10;
-            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            gameObject.transform.localScale = new Vector3(-.5f, .5f, .5f);
         }
 
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
             speed = 0;
         }
+        
+    }
 
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, 10f);
+        }
+        
+    }
 
+    void BackToMainMenu()
+    {
         if (Input.GetKeyDown(KeyCode.Backspace))
         {
             SceneManager.LoadScene(0);
         }
-    }
-
-    void FixedUpdate()
-    {
-
-        r2d.velocity = new Vector2(speed * inputX, speed * inputY);
-        //jump = false;
     }
 }
