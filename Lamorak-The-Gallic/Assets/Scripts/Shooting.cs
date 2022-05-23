f876465ac0ad5bff91f6ef904e7ec69a5fa4560f
Bernardo Castro <bernardoc544@gmail.com>
2022-05-23 12:00:52 -0400

    Trajectory Prediction

--- Lamorak-The-Gallic/Assets/Scripts/Shooting.cs
+++ Lamorak-The-Gallic/Assets/Scripts/Shooting.cs
@@ -5,29 +5,58 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Animator anim;
-    GameObject player;
+    public Vector2 aim;
+    public float force;
+    public GameObject circlePrefab;
+    public GameObject[] circles;
+    public int numberOfCircles;
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<GameObject>();
+        circles = new GameObject[numberOfCircles];
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerPos = transform.position;
+        aim = mousePos - playerPos;
+
         if (Input.GetMouseButtonDown(0))
         {
             anim.SetBool("mouseButtonClicked", true);
-            Vector2 bowPosition = transform.position;
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 direction = mousePosition - bowPosition;
-            transform.right = direction;
+            for (int i = 0; i < numberOfCircles; i++)
+            {
+                circles[i] = Instantiate(circlePrefab, transform.position, Quaternion.identity);   
+                circles[i].transform.position = pointingThePosition(i * 0.1f);
+                aimingBow();
+            }
             gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         else if (Input.GetMouseButtonUp(0))
         {
             anim.SetBool("mouseButtonClicked", false);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            for(int i = 0; i < numberOfCircles; i++)
+            {
+                Destroy(circles[i]);
+            }
+            
         }
-        
+    }
+
+
+    void aimingBow()
+    {
+        transform.right = aim;
+    }
+
+    Vector2 pointingThePosition(float t)
+    {
+        Vector2 currentPointPos = (Vector2)transform.position + (aim.normalized * force * t) + .5f * Physics2D.gravity * (t * t);
+        return currentPointPos;
     }
 }
