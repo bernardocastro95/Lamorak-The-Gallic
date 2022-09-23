463ea85f295fbcbd1e674d34b4a1379d25c14c4f
bernardocastro95 <bernardoc544@gmail.com>
2022-09-23 19:29:54 -0400

    Level 3 Player movement

---
+++ Lamorak-The-Gallic/Assets/Scripts/Knight.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    public float speed = 0;
+    Rigidbody2D r2d;
+    float inputX, inputY;
+    [SerializeField]
+    GameManager gm;
+    //public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //animator.SetFloat("GroundSpeed", Mathf.Abs(speed));
+
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+
+        Movement();
+        
+
+
+        if (gm.paused == true || gm.isGameOver == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        r2d.velocity = new Vector2(speed * inputX, speed * inputY);
+    }
+
+
+
+    void Movement()
+    {
+        if (gm.gameRunning)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                speed += 10;
+                gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+
+            if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                speed = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                speed += 10;
+                gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+
+            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                speed = 0;
+            }
+
+        }
+
+    }
+
+}
