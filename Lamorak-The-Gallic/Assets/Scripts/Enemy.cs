aad401f7b30eb159aa446dc18273bb0acbbbd226
bernardocastro95 <bernardoc544@gmail.com>
2022-10-17 14:29:13 -0400

    Enemy flips

--- Lamorak-The-Gallic/Assets/Scripts/Enemy.cs
+++ Lamorak-The-Gallic/Assets/Scripts/Enemy.cs
@@ -2,59 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Enemy : MonoBehaviour
 {
-    [HideInInspector]
-    public bool patrol;
     public Rigidbody2D r2d;
     public float speed;
+    public Transform player;
+    private SpriteRenderer sRenderer;
     public Animator animator;
-    public Transform groundChecker;
-    private bool turn;
-    public LayerMask groundLayer;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        patrol = true;
+        this.sRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (patrol)
+        animator.SetFloat("speed", Mathf.Abs(speed));
+        move();
+        this.sRenderer.flipX = player.transform.position.x > this.transform.position.x;
+
+        if(player.transform.position.x > this.transform.position.x)
         {
-            animator.SetFloat("speed", Mathf.Abs(speed));
-            move();
+            speed *= -1;
         }
-        flip(); 
+        
     }
 
-    private void FixedUpdate()
-    {
-        if (patrol)
-        {
-            turn = !Physics2D.OverlapCircle(groundChecker.position, 0.1f, groundLayer);
-        }
-    }
 
     void move()
     {
-        flip();
         r2d.velocity = new Vector2(speed * Time.fixedDeltaTime, r2d.velocity.y);
     }
-    void flip()
-    {
-        if(gameObject.transform.position.x == 0)
-        {
-            speed = speed * -1;
-            gameObject.transform.localScale = new Vector3(-1f, 1f, 1f);
 
-        }
-        else if(gameObject.transform.position.x == 7.8)
-        {
-            speed = speed * -1;
-            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-    }
 }
