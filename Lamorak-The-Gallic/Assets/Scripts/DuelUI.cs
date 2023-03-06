3fd63e2b23c5df6c6cf70c5f2da04b2b59648740
bernardocastro95 <bernardoc544@gmail.com>
2023-03-06 15:11:36 -0500

    Building up level 3 UI

--- Lamorak-The-Gallic/Assets/Scripts/DuelUI.cs
+++ Lamorak-The-Gallic/Assets/Scripts/DuelUI.cs
@@ -2,31 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DuelUI : MonoBehaviour
 {
-    [SerializeField]
-    GameObject[] playerLifes;
-    [SerializeField]
-    GameObject[] enemyLifes;
     [SerializeField]
     Text middleText;
     [SerializeField]
     Button gameButton;
     [SerializeField]
     Button mainMenuButton;
-    int lifes;
+    int playerLifes, enemyLifes;
     private GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
-        lifes = 3;
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
 
         if (gm == null)
         {
             Debug.LogError("No Game Manager");
         }
+        gameButton.onClick.AddListener(resumeGame);
+        gameButton.onClick.AddListener(restartGame);
+        mainMenuButton.onClick.AddListener(backtoMainMenu);
     }
 
     // Update is called once per frame
@@ -42,25 +41,50 @@ public class DuelUI : MonoBehaviour
 
     public void playerLifeManager()
     {
-        
-        lifes -= 1;
+        playerLifes = 3;
+        playerLifes -= 1;
+        if(playerLifes == 0)
+        {
+
+        }
 
     }
     public void enemyLifeManager()
     {
+        enemyLifes = 3;
+        enemyLifes -= 1;
+        if(enemyLifes < 0)
+        {
 
-        lifes -= 1;
+        }
 
     }
 
     public void pauseMenu()
     {
         Time.timeScale = 0;
-        middleText.text = "                               GAME PAUSED";
+        middleText.text = "                 GAME PAUSED";
         gameButton.gameObject.SetActive(true);
         gameButton.gameObject.GetComponentInChildren<Text>().text = "Resume";
         mainMenuButton.gameObject.SetActive(true);
         
 
+    }
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        middleText.text = "";
+        gameButton.gameObject.SetActive(false);
+        mainMenuButton.gameObject.SetActive(false);
+    }
+    public void restartGame()
+    {
+        Time.timeScale = 1;
+        middleText.text = "";
+        gameButton.gameObject.SetActive(false);
+    }
+    public void backtoMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
