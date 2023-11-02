f4816df6ae6d870e5f9a5dea78fd27f6c6215405
bernardocastro95 <bernardoc544@gmail.com>
2023-11-01 21:26:27 -0400

    You win scene

---
+++ Lamorak-The-Gallic/Assets/Scripts/FinalSceneButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FinalSceneButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Button exitButton, mainMenuButton;
+    
+
+    public void loadScene()
+    {
+        exitButton.onClick.AddListener(endGame);
+        mainMenuButton.onClick.AddListener(backToMainMenu);
+    }
+
+    public void endGame()
+    {
+        Application.Quit();
+    }
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
